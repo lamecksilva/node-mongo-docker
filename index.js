9e14fe0ef2d89331928378d5b4c062bddf9a4108
lamecksilva <lameck2@hotmail>
2019-02-18 09:15:30 -0300

    Adding gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://mongo:27017/testedb", { useNewUrlParser: true }).then(res => {
+  console.log("MongoDB connected")
+}).catch(err => {
+  console.log("Error in connection")
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log("Server running on port 8000");
+})
