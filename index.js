04ed47edffe9992e5c566186984c4683066346ba
lamecksilva <lameck2@hotmail>
2019-02-19 20:50:14 -0300

    Adding data/ stuff

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect("mongodb://mongo:27017/testedb", { useNewUrlParser: true }).then(res => {
-  console.log("MongoDB connected")
-}).catch(err => {
-  console.log("Error in connection")
-})
+mongoose
+  .connect("mongodb://mongo:27017/testedb", { useNewUrlParser: true })
+  .then(res => {
+    console.log("MongoDB connected");
+  })
+  .catch(err => {
+    console.log("Error in connection");
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -17,4 +20,4 @@ const port = 8000;
 
 app.listen(port, () => {
   console.log("Server running on port 8000");
-})
+});
